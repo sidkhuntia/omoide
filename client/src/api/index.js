15837c9df9329216e730395981a90f38d0112ed6
sidkhuntia <siddharthakhuntia@gmail.com>
2022-06-02 18:56:51 +0530

    images

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://omoides-backend.herokuapp.com/",
 });
 
 API.interceptors.request.use((req) => {
